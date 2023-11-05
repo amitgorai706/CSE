d0a0f92a567264f26052bef54168784454c21767
Dinanath Dash <dashdinanath056@gmail.com>
2023-11-05 10:55:14 +0530

    Update

--- AD/Assignment-4/Q3.java
+++ AD/Assignment-4/Q3.java
@@ -1,18 +1,16 @@
-import java.util.Arrays;
 public class Q3 {
     public static void main(String[] args) {
-        int[] arr = {5, 6, 4, 10, 9, 7, 8,  3, 2, 1};
-        System.out.println("Original array: " + Arrays.toString(arr));
-        Arrays.sort(arr);
-        reverseArray(arr);
-        System.out.println("Reversed array: " + Arrays.toString(arr));
-    }
-    public static void reverseArray(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n / 2; i++) {
-            int temp = arr[i];
-            arr[i] = arr[n - 1 - i];
-            arr[n - 1 - i] = temp;
+        int arr[]={5, 4, 3, 2, 1};
+        int count=0;
+        for (int i = 0; i < arr.length-1; i++) {
+            if (arr[i]>arr[i+1]) {
+                count++;
+            }
+        }
+        if (count==(arr.length-1)){
+            System.out.println("The array is in reversed order");
+        } else{
+            System.out.println("The array is not in reversed order");
         }
     }
 }
