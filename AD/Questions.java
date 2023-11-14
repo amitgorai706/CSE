4382cf784b0679d7a3338dfafcd2c94cf01d52da
Dinanath Dash <dashdinanath056@gmail.com>
2023-11-14 11:17:39 +0530

    Update

---
+++ AD/Questions.java
@@ -0,0 +1,12 @@
+//Assignment-6
+//Question 1: Find the first repeated entry in one array
+//Question 2: Print duplicate in a list
+//Question 3: Find missinf number in an arra
+//Question 4: Given an array of integers. Find the element pairs maximum number of and minimmum number of.
+
+//Assignment-7
+//Question 1: Creation of Linked list
+//Question 2: Insertion in linked list
+//Question 3: Deletion from linked list
+//Question 4: Traverse in linked list
+//Question 5: Reverse of linked list
