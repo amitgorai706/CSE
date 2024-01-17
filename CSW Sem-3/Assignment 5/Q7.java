66eee5076a1e4568efcaf8fd6d207afbe22cf403
Dinanath Dash <dashdinanath056@gmail.com>
2024-01-17 11:13:02 +0530

    Update

--- CSW Sem-3/Assignment 5/Q7.java
+++ CSW Sem-3/Assignment 5/Q7.java
@@ -1,7 +1,7 @@
 public class Q7 {
     public static void main(String[] args) {
         int x = 10;
-        int y = 20;
+        int y = 2;
         double pow = Math.pow(x, y);
         System.out.println("The power of " + x + " to the " + y + " is = " + pow);
     }
