9c9391cac0132af5bfd6b57e7aa64e40c044fd12
Dinanath Dash <dashdinanath056@gmail.com>
2024-03-30 09:32:38 +0530

    Add files via upload

---
+++ CSW Sem-4/Assignment 5/Q1.java
@@ -0,0 +1,13 @@
+public class Q1 {
+    public static void main(String[] args) {
+        String text = null;
+
+        try {
+            int length = text.length();
+        } catch (NullPointerException e) {
+            System.out.println("Can't operate  on a null object!");
+        }
+
+        System.out.println("Program continues after handling NullPointerException.");
+    }
+}
